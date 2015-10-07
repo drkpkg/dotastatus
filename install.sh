98b79688ad219a0d85f48f791cf6dc42aa995c19
drkpkg <daniel.uremix@gmail.com>
2015-10-07 14:03:59 -0400

    FIX'D: install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-DIR="/usr/share/dotastatus"
+DIR="/usr/share/dotastatus/"
+IMAGES="/usr/share/dotastatus/images/"
 BIN="/usr/bin"
-mkdir -p $DIR
-cp -r images $DIR
+mkdir -p $IMAGES
+cp images/* $IMAGES
 cp dotastatus.py $DIR
 cp indicator_gtk.py $DIR
 cp icon.png $DIR
 cp dotastatus-gtk $BIN
-
+echo "Dota status installed, now execute dotastatus-gtk"
